b4ae5b4236706af8faded625f2be2cc88667cd77
nneyen <nneyen.umana@gmail.com>
2022-12-15 11:38:22 +0100

    day_1

---
+++ challenge_solutions/day_1_data_types.py
@@ -0,0 +1,24 @@
+#----Challenge Solution day 1---# 
+
+#there are initial data given by hackerrank
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+#there is no need for variable declaration when writing in python so we will move to the next instruction
+
+
+# Read and save an integer, double, and String to your variables.
+x = int(input())
+y = float(input())
+z = str(input())
+
+
+# Print the sum of both integer variables on a new line.
+print (i + x)
+# Print the sum of the double variables on a new line.
+print (d + y)
+# Concatenate and print the String variables on a new line
+print (s + z)
+# The 's' variable above should be printed first.
